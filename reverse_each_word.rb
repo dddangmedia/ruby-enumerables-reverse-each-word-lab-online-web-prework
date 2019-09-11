c779af47a8a15d5260e1bb7574ac3882cdd650ee
Duong Dang <dddangmedia@gmail.com>
2019-09-11 18:40:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_string = string.split(" ")
   array = []
-  new_string.each
+  new_string.each {|n| n.reverse >> array}
 end
 
 reverse_each_word("what tht fuck")
