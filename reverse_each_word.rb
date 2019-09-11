c2263481fd3592b1ee095682e3add5f13c16f953
Duong Dang <dddangmedia@gmail.com>
2019-09-11 18:41:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_string = string.split(" ")
   array = []
-  new_string.each {|n| array << n.reverse}
+  new_string.collect {|n| array << n.reverse}
   array.join(" ")
 end
 
